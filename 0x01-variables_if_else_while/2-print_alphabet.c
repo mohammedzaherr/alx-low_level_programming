9371c82c42988a7a9d58a24a14d5c586fcef36ef
Mohammed Zaher <mohammedzaherr0@gmail.com>
2023-02-20 20:57:21 +0200

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 26; i++)
+	{
+		putchar((char)(i + 97));
+	}
+	putchar('\n');
+	return (0);
+}
