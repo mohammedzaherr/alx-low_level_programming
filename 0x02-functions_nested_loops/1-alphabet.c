4131eb8396e4766841c3ef62322031a5d0db1d6f
Mohammed Zaher <mohammedzaherr0@gmail.com>
2023-02-21 23:04:09 +0200

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet
+ * in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+		_putchar(i + 97);
+	_putchar('\n');
+}
+
