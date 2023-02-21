0bccf8955dd55aac259c680ba1f7e9eb2522c6b6
Mohammed Zaher <mohammedzaherr0@gmail.com>
2023-02-21 23:09:25 +0200

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabet in lowercase
+ * for 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+	
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(j + 97);
+		}
+		_putchar('\n');
+	}
+}
