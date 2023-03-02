14524b12b812f654b9b8328cae7524722932719b
Mohammed Zaher <mohammedzaherr0@gmail.com>
2023-03-03 00:27:01 +0200

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @s: the input string
+ * Return: the string in uppercase
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= 97 && *(s + i) <= 122)
+			*(s + i) -= 32;
+		i++;
+	}
+	return (s);
+}
